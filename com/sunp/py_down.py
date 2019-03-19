4b7214b2f3863a6f77230aab74f7a6a762de4fe7
sunpeng <sunpeng1@souche.com>
2019-03-19 11:02:55 +0800

    selenium不打开浏览器

--- com/sunp/py_down.py
+++ com/sunp/py_down.py
@@ -4,10 +4,14 @@ import time
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 def main():
-  browser = webdriver.Chrome()
-  # browser.get("https://music.163.com/#/playlist?id=2593304089")
+  chrome_options = Options()
+  chrome_options.add_argument('--headless')
+  chrome_options.add_argument('--disable-gpu')
+  browser = webdriver.Chrome(options=chrome_options)
   browser.get("https://music.163.com/#/discover/toplist?id=3778678")
   browser.switch_to.frame('contentFrame')
 
@@ -31,13 +35,10 @@ def down(song,name):
   id=song.split("=")[1]
   name=name.replace("/","|")
   url = 'http://music.163.com/song/media/outer/url?id='+id+'.mp3'
-  path = './music2/'+str(name)+'.mp3'
+  path = './music/'+str(name)+'.mp3'
   user_agent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.82 Safari/537.36'
   headers = {'User-Agent':user_agent,'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8','Upgrade-Insecure-Requests':'1'}
   response = requests.get(url,headers=headers)
-  # response.encoding = 'utf-8'
-  # html = response.text
-  # print(html)
   with open(path, 'wb') as f:
     f.write(response.content)
     f.flush()
